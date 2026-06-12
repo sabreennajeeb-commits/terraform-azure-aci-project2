fdb3625ae903933782d1a8763209b0feaaed3299
Retaj Fadhl <retajfadhl@gmail.com>
2026-06-12 11:26:19 +0200

    Add Terraform configuration for Azure ACI

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "sabreen-ritaj-rahaf-proj2-aci-rg"
+}
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+  default     = "swedencentral"
+}
+
+variable "container_group_name" {
+  description = "Name of the Azure Container Instance"
+  type        = string
+  default     = "sabreen-ritaj-rahaf-proj2-aci"
+}
+
+variable "dns_name_label" {
+  description = "DNS label for the Azure Container Instance"
+  type        = string
+  default     = "sabreen-ritaj-rahaf-project2"
+}
+
+variable "docker_image" {
+  description = "Docker Hub image used by Azure Container Instance"
+  type        = string
+  default     = "DOCKERHUB_USERNAME/cloudscale-project2:latest"
+}
+
+variable "student_name" {
+  description = "Student or team name used in Azure tags"
+  type        = string
+  default     = "Sabreen-Ritaj-Rahaf"
+}
